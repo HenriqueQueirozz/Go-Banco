02c2c7ff2a23cbeade24474bbb55b3849d3efb42
Henrique Queiroz de Paula <henrique.queiroz@contabilivre.com.br>
2023-03-17 13:05:45 -0300

    [2023-03-17] - Formas de declarar uma variável struct e indexação de variáveis

--- main.go
+++ main.go
@@ -9,7 +9,22 @@ type ContaCorrente struct {
 	saldo 			float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	}else{
+		return "Saldo insuficiente "
+	}
+}
+
 func main() {
-	contaDoHenrique := ContaCorrente{titular: "Henrique", numeroAgencia: 589, numeroConta: 123456, saldo: 300.0}
-	fmt.Println(contaDoHenrique)
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia'"
+	contaDaSilvia.saldo = 500
+	 
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDaSilvia.Sacar(300))
+	fmt.Println(contaDaSilvia.saldo)
 }
